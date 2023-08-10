e3cdb4cfd2095489783cd891c36776025c046bcd
Mary <luga_vmv@mail.ru>
2023-08-10 21:55:25 +0600

    fist file

--- venv/Lib/bot.py
+++ venv/Lib/bot.py
@@ -9,6 +9,7 @@ id = "1657110493"
 def send(text):
     url = "https://api.telegram.org/bot'+bot+'/sendMessage?chat_id='+id+'&text='+text+'"
     resp = requests.get(url)
-    r= resp.json()
+    r = resp.json()
 
 send("bitcoin")
+print("hi")
