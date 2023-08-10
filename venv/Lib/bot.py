34219c090a23dd38545a2160be58b3a5377848bb
Mary <luga_vmv@mail.ru>
2023-08-10 18:42:57 +0600

    fist file

---
+++ venv/Lib/bot.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+token = "6080573579:AAFQToJ_NQNHhHAQ24vJD85FZbSkFZPxzC8"
+id = "1657110493"
+
+def send(text):
+    url = "https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+id+'&text='+text+'"
+    resp = requests.get(url)
+    r= resp.json()
+    return
