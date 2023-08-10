5eefb49d4e293479a46c47b38e8c39f14b5a2fc8
Mary <luga_vmv@mail.ru>
2023-08-10 21:39:50 +0600

    fist file

--- venv/Lib/bot.py
+++ venv/Lib/bot.py
@@ -1,11 +1,13 @@
+import telebot
+bot = telebot.TeleBot("6080573579:AAFQToJ_NQNHhHAQ24vJD85FZbSkFZPxzC8")
 import requests
 import json
 
-token = "6080573579:AAFQToJ_NQNHhHAQ24vJD85FZbSkFZPxzC8"
+#token = "6080573579:AAFQToJ_NQNHhHAQ24vJD85FZbSkFZPxzC8"
 id = "1657110493"
 
 def send(text):
-    url = "https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+id+'&text='+text+'"
+    url = "https://api.telegram.org/bot'+bot+'/sendMessage?chat_id='+id+'&text='+text+'"
     resp = requests.get(url)
     r= resp.json()
 
